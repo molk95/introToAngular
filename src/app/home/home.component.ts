820dc1d7e55a3e86935dde2996af88615d86c5aa
molk.saouabi <molk.saouabi@gmail.com>
2024-11-04 19:14:12 +0100

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  users= [
+    {
+      id: 0,
+      fullName: "hamma hamma",
+      age: 19,
+    },
+    {
+      id: 1,
+      fullName: "foulen fouleni",
+      age: 29,
+    },
+    {
+      id: 2,
+      fullName: "foulen fouleni",
+      age: 5,
+    }
+  ];
+
+show(){
+  alert('Hello human')
+}
+}
